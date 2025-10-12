3f99ded745c6f43b88abe69e315f1bba46b3f74d
Peter Torki <pjturki@gmail.com>
2025-10-13 01:10:07 +0300

    done login

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Auth {
-  
+  private baseUrl = 'https://university-api-ten.vercel.app/auth';
+  constructor(private http: HttpClient) {}
+
+  register(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/signup`, data);
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, data);
+  }
 }
